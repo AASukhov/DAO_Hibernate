8aa85105b25f259db8160ad2f5e85821fde2086a
Andrey <arkhonfills@list.ru>
2022-10-04 22:57:12 +0300

    spring-method add

--- src/main/java/com/example/dao/SecurityConfiguration.java
+++ src/main/java/com/example/dao/SecurityConfiguration.java
@@ -3,12 +3,14 @@ package com.example.dao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder encoder() {
@@ -20,11 +22,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("Andrey")
                 .password(encoder().encode("12345678"))
-                .authorities("read", "write")
+                .roles("READ", "WRITE", "DELETE")
                 .and()
                 .withUser("Vasily")
                 .password("{noop}qwerty")
-                .authorities("read");
+                .authorities("READ")
+                .and()
+                .withUser("Petr").password(encoder().encode("qwerty12"))
+                .roles("WRITE");
     }
 
     @Override
